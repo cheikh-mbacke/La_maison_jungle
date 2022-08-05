976371bdfd3d08bee55e641f315a6aff34d42cb5
Mbacke <mbackecheikhouna7@gmail.com>
2022-08-05 19:35:18 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Banner from './Banner/Banner'
+import Cart from './Cart/Cart';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <Banner />
+        <Cart />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,5 @@
+
+export default function Banner() {
+    const title = "La maison jungle"
+    return (<h1>{title}</h1>)
+}
